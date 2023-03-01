eddf0a136f2b0037f76d5341a32720aec57d17e2
حسان ونس <36311551+Hassan-Wanas@users.noreply.github.com>
2023-03-01 14:42:01 +0200

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        Black1: '#181719',
+        Gray6: '#F2F2F2'
+      }
+    },
+    fontFamily: {
+      Montserrat: 'Montserrat',
+      CrimsonPro: 'Crimson Pro',
+      Lora: 'Lora',
+    }
+  },
+  plugins: [],
+}
