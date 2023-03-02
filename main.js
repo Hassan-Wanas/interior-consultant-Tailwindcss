b235658f42cd6784fcafb3cdce43ffca3c88c536
حسان ونس <36311551+Hassan-Wanas@users.noreply.github.com>
2023-03-02 13:46:23 +0200

    add show & hide to menu

--- main.js
+++
@@ -1,3 +0,0 @@
-document.querySelector('fa-bars').addEventListener('click',() => {
-  document.querySelector('menu').classList.add('block');
-})
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@
 module.exports = {
   content: [
     './index.html',
-    './main.js'
   ],
   theme: {
     extend: {
